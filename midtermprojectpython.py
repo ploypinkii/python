dd6f5b89daddf5375e0d8f408dc9a6db846092ed
Ploychomphu Rungphisitchai <ployp@kkumail.com>
2021-03-15 19:04:05 +0700

    update

---
+++ midtermprojectpython.py
@@ -0,0 +1,76 @@
+ntrack = []
+i = 0
+def index():
+    print('ขนส่งของพวยพิ้งและโบ๊ทๆ')
+    print('[1] เพิ่มพัสดุ\n[2] อัพเดทพัสดุ\n[3] เช็คเลขแทร็กกิ้ง\n[x] ออกจากโปรแกรม')
+
+def add():
+    sname = input('ชื่อผู้ส่งพัสดุ :')
+    gname = input('ชื่อผู้รับพัสดุ :')
+    stype = input('รูปแบบการส่ง\n[1] ส่งด่วน EMS\n[2] ส่งธรรมดา REG\nเลือกรูปแบบที่ต้องการส่ง : ')
+    if stype == '1':
+        priceems()
+        emstracking()
+    else:
+        pricereg()
+        regtracking()
+
+def priceems():
+    print('สาขาปลายทาง\n[3] นครราชสีมา\n[4] กรุงเทพ')
+    kk = int(input('เลือกสาขาปลายทาง : '))
+    priceems =float(input('weight of package (kg) :'))
+    if priceems <= 1 :
+        Totalems = (priceems + 30) + (7*kk)
+    elif 1 < priceems <= 10 :
+        Totalems = (priceems + 60) + (7*kk)
+    else:
+        Totalems = (priceems + 100) + (7*kk)
+    print(str(int(Totalems)+1) +' baht')
+    
+def pricereg():
+    print('สาขาปลายทาง\n[3] นครราชสีมา\n[4] กรุงเทพ')
+    kk = int(input('เลือกสาขาปลายทาง : '))
+    priceems =float(input('weight of package (kg) :'))
+    if priceems <= 1 :
+        Totalems = (priceems + 15) + (7*kk)
+    elif 1 < priceems <= 10 :
+        Totalems = (priceems + 15) + (7*kk)
+    else:
+        Totalems = (priceems + 50) + (7*kk)
+    print(str(int(Totalems)+1) +' baht')
+
+def emstracking():
+    import random
+    for i in range(10):
+        atrack = (random.randint(0,9))
+        ntrack.append(atrack)
+    a=0
+    b=1
+    for i in range(10):
+        a=a+(ntrack[i]*(b))
+        b=b*10
+    print('EMS' + str(a) + 'TH')
+
+def regtracking():
+    import random
+    for i in range(10):
+        atrack = (random.randint(0,9))
+        ntrack.append(atrack)
+    a=0
+    b=1
+    for i in range(10):
+        a=a+(ntrack[i]*(b))
+        b=b*10
+    print('REG' + str(a) + 'TH')
+
+#finalprogram
+while True:
+    index()
+    choose = input('choose menu : ')
+    if choose == '1':
+        a = int(input('number of box :'))
+        while(i<a):
+            add()
+            i+=1
+    elif choose == '2':
+        print('0')
--- week5.2.py
+++ week5.2.py
@@ -1,7 +1,39 @@
-#ใบงานที่ 5.2 นางสาวพลอยขชมพู รุ่งพิสิฐไชย 633050135-6
-def menu():
-    print('แสดงรายการสินค้า [s]\nเพิ่มรายการสินค้า[a]\nออกจากระบบ[x]')
-    choose = print('กรุณาเลือกคำสั่ง :')
-    #choose = choose.lower()
+#ใบงานที่ 5.2 นางสาวพลอยขชมพู รุ่งพิสิฐไชย 633050135-6num = 0 
+products_ =[]
+rates_ = []
+class shop :
+    def __init__(self,products,rates):
+        self.products = products
+        self.rates = rates
+    def showshop(self):
+        print(i+1,'.',self.products,self.rates,' บาท')
+    def show():
+        print('-'*20,'ร้านนายดอม','-'*20)
+        print('\tแสดงรายการสินค้า [a]\n\tเพิ่มรายการสินค้า[s]\n\tออกจากระบบ[x]')
 
-menu()
+while True :
+    shop.show() 
+    menu = input('กรุณาเลือกคำสั่ง :   ')
+    if menu == 'a':
+        for i in range(num):
+            x = shop(products_[i],rates_[i])
+            x.showshop()
+    if menu == 's' :
+        while True :
+            print('เพิ่มรายการสินค้า หากต้องการยกเลิกให้กรอก exit')
+            g = input('เพิ่มชื่อสินค้า :\t')
+            if  g == 'exit' :
+                break
+            else:
+                products_.append(g) 
+                r = input('ใส่ราคาของ'+g+': ')
+                rates_.append(r)
+                print('-'*5+'[บันทึกข้อมูลสินค้าแล้ว]'+'-'*5)
+                num +=1
+                yn = input('ต้องการเพิ่มสินค้าอีกหรือไม่(y/n): ')
+                if yn == 'n' :
+                    break
+    if menu == 'x' :
+        yn = input('ต้องการออกจากระบบหรือไม่(y/n): ')
+        if yn == 'y' :
+            break
